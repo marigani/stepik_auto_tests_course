bffe5a73650720d3cc44c4542dfc7b58c9ac8c76
Ganieva Marina <ganievamv@gmail.com>
2022-04-11 11:05:44 +0400

    added lesson 2_2_step7

---
+++ lesson_2_2_step7.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+import math
+import time
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "http://SunInJuly.github.io/execute_script.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element_by_id('input_value')
+    x = x_element.text
+    y = calc(x)
+    
+    input1 = browser.find_element_by_id("answer")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", input1)
+    input1.send_keys(y)
+    
+
+    option1 = browser.find_element_by_id("robotCheckbox")
+    option1.click()
+    option2 = browser.find_element_by_id("robotsRule")
+    option2.click()
+    # Отправляем заполненную форму
+    button = browser.find_element_by_css_selector(".container button")
+    button.click()
+
+       
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
