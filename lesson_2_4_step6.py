c35ab109bfa9d1c9370f89b8872ea80b4207928e
Ganieva Marina <ganievamv@gmail.com>
2022-04-11 11:12:59 +0400

    added lesson 2_4_step6

---
+++ lesson_2_4_step6.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+
+
+browser = webdriver.Chrome()
+browser.implicitly_wait(5)
+browser.get("http://suninjuly.github.io/cats.html")
+
+
+browser.find_element_by_id("button")
+
